f0f1add62ecc38ab728e36472d8da03d821dbbec
ulug2002 <ulugbek_it@mail.ru>
2023-05-08 14:32:35 +0300

    d4

--- get_first_row.py
+++ get_first_row.py
@@ -7,6 +7,8 @@ def get_first_row(data):
         list: First row.
    """
    
-   return
+   return data.split("\n")[1].split(',')
 
 # Read the csv file
+data = open('data.csv').read()
+print(get_first_row(data))
